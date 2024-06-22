fe188590e28d1dad17e7c546fd16d6158504a3ea
chrimp <60501603+chrimp@users.noreply.github.com>
2024-06-23 06:08:04 +0900

    Initial Commit

---
+++ eyeTracker/eyeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  eyeTrackerApp.swift
+//  eyeTracker
+//
+//  Created by 김태우 on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct eyeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
